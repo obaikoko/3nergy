c0088d39e452661702ae280f89ab49128132a17f
Jesse Obinna <jesseobinna7@gmail.com>
2025-09-22 16:21:32 +0100

    added logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,17 +1,19 @@
-import Link from 'next/link';
-import { APP_NAME } from '@/lib/constants';
-import Menu from '@/components/shared/header/menu';
-import ModeToggle from './shared/header/mode-toggle';
+import Link from "next/link";
+import { APP_NAME } from "@/lib/constants";
+import Menu from "@/components/shared/header/menu";
+import ModeToggle from "./shared/header/mode-toggle";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className='fixed top-0 left-0 w-full z-50 bg-background text-foreground'>
-      <div className='mx-auto max-w-screen-xl px-4 flex justify-between items-center py-2'>
-        <Link href='/' className='flex items-center space-x-2'>
-          <span className='text-2xl font-bold bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent'>
-            {APP_NAME}
+    <header className="fixed top-0 left-0 w-full z-50 bg-background text-foreground">
+      <div className="mx-auto max-w-screen-xl px-4 flex justify-between items-center py-2">
+        <Link href="/" className="flex items-center space-x-2">
+          <span className="text-2xl font-bold bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent">
+            <Image alt="Cesol3nergy" width={80} height={40} src="/logo.png" />
           </span>
         </Link>
+
         <ModeToggle />
         <Menu />
       </div>
